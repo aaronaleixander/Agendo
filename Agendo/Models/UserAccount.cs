66570fba52871be68f0609225933aa201f317fe0
aau19 <aau19@DESKTOP-M0K5TMI>
2019-11-15 09:01:49 -0800

    Add project files.

---
+++ Agendo/Models/UserAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agendo.Models
+{
+    public class UserAccount
+    {
+        public int UserID { get; set; }
+
+        /// <summary>
+        /// The users First Name
+        /// </summary>
+        [Required]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The users last name
+        /// </summary>
+        [Required]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The users chosen username from site use.
+        /// </summary>
+        [Required]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Users E - Mail Address
+        /// </summary>
+        [Required]
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// User Passwword
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Users Date of Birth
+        /// </summary>
+        [Display(Name ="Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
