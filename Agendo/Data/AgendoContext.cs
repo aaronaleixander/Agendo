d8b810fcb6f7374d61b32bbd33e06cd9087b278e
aau19 <aau19@DESKTOP-M0K5TMI>
2019-11-15 09:22:37 -0800

    Created UserAccount class and Agendo Database Context class.

---
+++ Agendo/Data/AgendoContext.cs
@@ -0,0 +1,19 @@
+﻿using Agendo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agendo.Data
+{
+    public class AgendoContext : DbContext
+    {
+        public AgendoContext(DbContextOptions<AgendoContext> options) 
+            : base(options)
+        {
+
+        }
+        public DbSet<UserAccount> Users  { get; set; }
+    }
+}
---
+++ Agendo/Data/UserAccountDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agendo.Data
+{
+    public static class UserAccountDb
+    {
+        public async static Task<UserAccountDb> Add(AgendoContext context, UserAccountDb user)
+        {
+            
+            
+        }
+    }
+}
